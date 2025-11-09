eb5fd67e19457d945dbe4bef9a5385dd585aa249
linhngan2209 <nganptlhe160415@fpt.edu.vn>
2025-11-09 21:00:15 +0700

    add quizz

---
+++ src/app/quizzes/page.tsx
@@ -0,0 +1,12 @@
+import QuizSection from '@/components/Quizzes/QuizzLevel';
+import type { NextPage } from 'next';
+
+const Quizz: NextPage = () => {
+  return (
+    <div>
+        <QuizSection />
+    </div>
+  );
+};
+
+export default Quizz;
